2b698494abff31e4b172b017f2ad23aae20b7e9a
dalbitresb12 <diego.albitres@gmail.com>
2023-10-28 03:08:47 -0500

    chore(lint): added eslint initial config

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import { AppEnv, createRouter } from "./utils/router";
 
 const router = createRouter();
 
-router.get('/', (request, env, ctx) => {
+router.get('/', () => {
   return { success: true, message: 'Hello, world!' };
 });
 
 export default {
-  fetch(request: Request, env: AppEnv, ctx: ExecutionContext) {
+  fetch(request: Request, env: AppEnv, ctx: ExecutionContext): Promise<Response> {
     return router
       .handle(request, env, ctx)
       .then(json)
