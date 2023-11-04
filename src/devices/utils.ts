3fe4cde6e9d438c3493332215c52d0d22b79f932
dalbitresb12 <diego.albitres@gmail.com>
2023-11-04 06:34:15 -0500

    refactor(kv): created utils for KV key generation

--- src/devices/utils.ts
+++ src/devices/utils.ts
@@ -1,32 +1,27 @@
 import { StatusError } from "itty-router";
 
 import { Device } from "../models/device";
-
-const separator = ":";
+import { createKey, splitKey } from "../utils/kv-keys";
 
 interface DeviceKey {
   userId: string;
   deviceId: string;
 }
 
-function createKey(values: string[]) {
-  return values.join(separator);
-}
-
-export const deviceKeyPrefix = "device";
+const deviceKeyPrefix = "device";
 
 export function createDeviceKey(userId: string, deviceId: string): string {
   return createKey([deviceKeyPrefix, userId, deviceId]);
 }
 
-export const serialNumberKeyPrefix = "serial";
+const serialNumberKeyPrefix = "serial";
 
 export function createSerialNumberKey(serialNumber: string): string {
   return createKey([serialNumberKeyPrefix, serialNumber]);
 }
 
 export function parseDeviceKey(key: string): DeviceKey {
-  const split = key.split(separator);
+  const split = splitKey(key);
   if (split[0] !== deviceKeyPrefix) {
     throw new StatusError(500, `Found an invalid prefix while parsing a device key: ${split[0]}`);
   }
